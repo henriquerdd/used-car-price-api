a8f9bb59b9eab0fb545b31468f937ca9790ba0ce
henrique.deus <henrique.deus@memed.com.br>
2021-10-01 16:11:05 -0300

    Implementing user's crud routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, Body, Param, Query, Post, Get, Patch, Delete } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -9,5 +10,25 @@ export class UsersController {
   @Post('/signup')
   createUser (@Body() body: CreateUserDto) {
     this.service.create(body)
+  }
+
+  @Get('/:id')
+  findUser (@Param('id') id: string) {
+    return this.service.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers (@Query('email') email: string) {
+    return this.service.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser (@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.service.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  removeUser (@Param('id') id: string) {
+    return this.service.remove(parseInt(id));
   }
 }
